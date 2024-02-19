ad3fdf687ed55fb37a437a1136bf9ed9e6f1d602
Deepanshu Pratik <65304599+DeepanshuPratik@users.noreply.github.com>
2024-02-20 04:39:01 +0530

    Update generate-strings-csv.py

--- generate-strings-csv.py
+++ generate-strings-csv.py
@@ -7,12 +7,15 @@ def generate_random_string(length=10):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 def generate_csv_with_random_strings(filename, num_strings=5, string_length=10):
-    """Generate a CSV file containing random strings."""
-    with open(filename, 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(['Random String'])
-        for _ in range(num_strings):
-            csv_writer.writerow([generate_random_string(string_length)])
+    with open(file_path, 'r') as f:
+        file_content = f.read()
+    print(file_content)
+    # """Generate a CSV file containing random strings."""
+    # with open(filename, 'w', newline='') as csvfile:
+    #     csv_writer = csv.writer(csvfile)
+    #     csv_writer.writerow(['Random String'])
+    #     for _ in range(num_strings):
+    #         csv_writer.writerow([generate_random_string(string_length)])
 
 if __name__ == "__main__":
     generate_csv_with_random_strings('https://raw.githubusercontent.com/DeepanshuPratik/ImageAnnotator/main/random_strings.csv
